7aedc82ab4f41acd73be057bc0aa60e30b8f16bf
Strong2197 <oleksandrkostishyn@gmail.com>
2026-02-09 22:38:31 +0200

    SECRET_KEY

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
-
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-78@zrab0x=2m7jjwnx*$6g6kir=1^nnzun@c*6r=^gb@usq+8('
+SECRET_KEY = os.getenv('SECRET_KEY', 'insecure-default-key-change-me')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = ['ifit.pythonanywhere.com', '*']
 
