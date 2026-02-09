1fca3de0dd78906dc5f38f38ccfd67c5d6dd6f79
unknown <oleksandrkostishyn@gmail.com>
2026-02-09 22:29:39 +0200

    Changes for Service

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-78@zrab0x=2m7jjwnx*$6g6kir=1^nnzun@c*6r=^gb@usq+8('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['ifit.pythonanywhere.com', '*']
 
