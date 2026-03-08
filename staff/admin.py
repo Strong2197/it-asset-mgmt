29de7f67c19543a7ca393efaf7ff5620117c3bec
Strong2197 <oleksandrkostishyn@gmail.com>
2026-03-08 21:51:04 +0200

    a

--- staff/admin.py
+++ staff/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Employee, CareerHistory
 
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+
+    # Які колонки показувати в списку
+    list_display = (
+        'id',
+        'full_name',
+        'position',
+        'department',  # Наша кастомна колонка з вмістом
+        'is_dismissed',
+        'created_at'  # Наш кольоровий статус
+    )
+    ordering = ('-id',)
 # Register your models here.
